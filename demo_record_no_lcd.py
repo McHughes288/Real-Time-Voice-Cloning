77b5d7ed5064963e6081a0551946a366aa4d4ed7
John Hughes <johnh@speechmatics.com>
2019-12-05 14:28:41 +0000

    remoing recording since on remote

--- demo_record_no_lcd.py
+++ demo_record_no_lcd.py
@@ -9,6 +9,7 @@ import librosa
 import argparse
 import torch
 import sys
+import wavio
 
 if __name__ == '__main__':
     ## Info & args
@@ -24,6 +25,9 @@ if __name__ == '__main__':
     parser.add_argument("-v", "--voc_model_fpath", type=Path, 
                         default="inference/vocoder/saved_models/pretrained/pretrained.pt",
                         help="Path to a saved vocoder")
+    parser.add_argument("--low_mem", action="store_true", help=\
+        "If True, the memory used by the synthesizer will be freed after each use. Adds large "
+        "overhead but allows to save some GPU memory for lower-end GPUs.")
     parser.add_argument("--no_sound", action="store_true", help=\
         "If True, audio won't be played.")
     args = parser.parse_args()
@@ -40,63 +44,59 @@ if __name__ == '__main__':
     synthesizer = Synthesizer(args.syn_model_dir.joinpath("taco_pretrained"), low_mem=args.low_mem)
     vocoder.load_model(args.voc_model_fpath)
     
-    print("Interactive generation loop")
     num_generated = 0
-    fs=44100
-    rec_duration = 10  # seconds
-    while True:
-        try:
-            
+    # fs=44100
+    # rec_duration = 10  # seconds
+    
+    # myrecording = sd.rec(rec_duration * fs, samplerate=fs, channels=1, dtype='float64')
+    # print("Recording Audio")
+    # sd.wait()
 
-            # Get the reference audio filepath
-            lcd.clear()
-            lcd.message = "Speak into the microphone\n Time left: %is" % rec_duration  # seconds
+    # sd.play(myrecording, fs)
+    # sd.wait()
 
-            
-            myrecording = sd.rec(rec_duration * fs, samplerate=fs, channels=1, dtype='float64')
-            print("Recording Audio")
-            sd.wait()
-            
-            ## Computing the embedding
-            preprocessed_wav = encoder.preprocess_wav(myrecording, fs)
-            print("Loaded file succesfully")
-            
-            # Then we derive the embedding.
-            embed = encoder.embed_utterance(preprocessed_wav)
-            print("Created the embedding")
-            
-            
-            ## Generating the spectrogram
-            text = "Hello, this is a synthesized version of John's voice"
-            
-            # The synthesizer works in batch, so you need to put your data in a list or numpy array
-            specs = synthesizer.synthesize_spectrograms([text], [embed])
-            spec = specs[0]
-            print("Created the mel spectrogram")
-            
-            
-            ## Generating the waveform
-            print("Synthesizing the waveform")
-            generated_wav = vocoder.infer_waveform(spec)
-            
-            
-            ## Post-generation
-            generated_wav = np.pad(generated_wav, (0, synthesizer.sample_rate), mode="constant")
-            
-            # Play the audio (non-blocking)
-            if not args.no_sound:
-                sd.stop()
-                sd.play(generated_wav, synthesizer.sample_rate)
-                
-            # Save it on the disk
-            fpath = "output/demo_output_%02d.wav" % num_generated
-            print(generated_wav.dtype)
-            librosa.output.write_wav(fpath, generated_wav.astype(np.float32), 
-                                     synthesizer.sample_rate)
-            num_generated += 1
-            print("\nSaved output as %s\n\n" % fpath)
-            
-        except Exception as e:
-            print("Caught exception: %s" % repr(e))
-            print("Restarting\n")
+    # rec_path = "output/demo_record_%02d.wav" % num_generated
+    # wavio.write(rec_path, myrecording, fs)
+
+    in_fpath = "demo/johns_voice3.wav"
+    
+    ## Computing the embedding
+    preprocessed_wav = encoder.preprocess_wav(in_fpath)
+    print("Loaded file succesfully")
+    
+    # Then we derive the embedding.
+    embed = encoder.embed_utterance(preprocessed_wav)
+    print("Created the embedding")
+    
+    
+    ## Generating the spectrogram
+    text = "Hello, this is a synthesized version of John's voice, I hope that it is very impressive, I like drones, this is a much longer sentence to see how the model deals with it."
+    
+    # The synthesizer works in batch, so you need to put your data in a list or numpy array
+    specs = synthesizer.synthesize_spectrograms([text], [embed])
+    spec = specs[0]
+    print("Created the mel spectrogram")
+    
+    
+    ## Generating the waveform
+    print("Synthesizing the waveform")
+    generated_wav = vocoder.infer_waveform(spec)
+    
+    
+    ## Post-generation
+    generated_wav = np.pad(generated_wav, (0, synthesizer.sample_rate), mode="constant")
+    
+    # Play the audio (non-blocking)
+    if not args.no_sound:
+        sd.stop()
+        sd.play(generated_wav, synthesizer.sample_rate)
+        
+    # Save it on the disk
+    fpath = "output/demo_output_%02d.wav" % num_generated
+    print(generated_wav.shape)
+    print(synthesizer.sample_rate)
+    librosa.output.write_wav(fpath, generated_wav.astype(np.float32), 
+                                synthesizer.sample_rate)
+    num_generated += 1
+    print("\nSaved output as %s\n\n" % fpath)
         
